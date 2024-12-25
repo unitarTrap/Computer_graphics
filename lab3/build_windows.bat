1811fe2dd9682ac693d07fa73f9b02e1eca0645c
unitarTrap <125769808+unitarTrap@users.noreply.github.com>
2024-12-25 07:21:20 +0300

    Update build_windows.bat

--- lab3/build_windows.bat
+++ lab3/build_windows.bat
@@ -1,2 +1,9 @@
 @echo off
-pyinstaller --onefile --add-data "resources;resources" main.py
+pyinstaller --noconfirm --onefile --windowed ^
+--name "AlienInvasion" ^
+--icon=resources/Images/ship.bmp ^
+--add-data "resources/Images;resources/images" ^
+--add-data "resources/sounds;resources/sounds" ^
+--add-data "resources/sounds;resources/saves" ^
+alien_invasion.py
+pause
